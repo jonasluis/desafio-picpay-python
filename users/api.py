d54b38aaee670c8a8a7ce0242cc79836000a5a02
jonasluis <jonasluis66@gmail.com>
2025-02-15 09:23:41 -0300

    criando um post para rota user

---
+++ users/api.py
@@ -0,0 +1,11 @@
+from ninja import Router
+
+# Importa o objeto Router para definir rotas no aplicativo
+users_router = Router()
+
+# Define uma rota POST na raiz do users_router ('/')
+# A resposta esperada tem status 200 e retorna um dicionário
+@users_router.post('/', response={200: dict})
+def create_user(request):
+    # Retorna um dicionário com a chave 'ok' (possível erro, pois 'ok' não foi definido)
+    return {'ok': ok}  # Isso pode gerar um NameError se 'ok' não estiver definido

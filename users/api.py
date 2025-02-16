9b79cc10316fd250a2a85cc1dfe99c0a865c2f68
jonasluis <jonasluis66@gmail.com>
2025-02-16 08:08:35 -0300

    adicionado forma de salvar usuarios com senha crpiptografada

--- users/api.py
+++ users/api.py
@@ -1,11 +1,17 @@
 from ninja import Router
-
+from .schemas import UserSchema
+from .models import User
+from django.contrib.auth.hashers import make_password
 # Importa o objeto Router para definir rotas no aplicativo
 users_router = Router()
 
 # Define uma rota POST na raiz do users_router ('/')
 # A resposta esperada tem status 200 e retorna um dicionário
 @users_router.post('/', response={200: dict})
-def create_user(request):
+def create_user(request, user: UserSchema):
+    user = User(**user.dict())
+    user.password = make_password(user.password)
+    user.save()
     # Retorna um dicionário com a chave 'ok' (possível erro, pois 'ok' não foi definido)
     return {'ok': 'ok'}  # Isso pode gerar um NameError se 'ok' não estiver definido
+
